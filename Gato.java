16b131b2f4925858c4ab961986f116e1bec61006
HR-flash <124082267+HR-flash@users.noreply.github.com>
2023-02-22 19:20:49 -0600

    Completo

---
+++ Gato.java
@@ -0,0 +1,14 @@
+public class Gato {
+    public static void main(String[] args) {
+        String lh = "-----";
+        String linea = "|";
+        
+        System.out.println(lh);
+        System.out.println(linea + "   " + linea);
+        System.out.println(linea + "   " + linea);
+        System.out.println(linea + "   " + linea);
+        System.out.println(lh);
+        
+        
+    }
+}
