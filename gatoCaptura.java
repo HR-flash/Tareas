12782240c97d015d6d46c8ae70983f98bb947448
HR-flash <124082267+HR-flash@users.noreply.github.com>
2023-03-01 18:29:18 -0600

    FALTA CAPTURAR E IMPRIMIR LOS DATOS

--- gatoCaptura.java
+++ gatoCaptura.java
@@ -1,1 +1,17 @@
-p
+import java.util.Scanner;
+
+public class gatoCaptura {
+
+    public static void main(String[] args) {
+        
+        //STRING Y TODO ESO 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Introduce la cadena de nueve caracteres: ");
+        String cadena = scanner.nextLine();
+        String linea = "|";
+        String lh = "-----";
+        System.out.println("\n"+cadena);
+
+        //CAPTURA DE LOS DAROS
+    }
+}
