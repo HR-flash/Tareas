ea3a49178e5b418ce39499e2ac45234f7f98ba31
HR-flash <124082267+HR-flash@users.noreply.github.com>
2023-03-03 19:17:10 -0600

    Falta ponerle un limitante

--- gatoCaptura.java
+++ gatoCaptura.java
@@ -13,5 +13,17 @@ public class gatoCaptura {
         System.out.println("\n"+cadena);
 
         //CAPTURA DE LOS DAROS
+        char[][] tablero = {
+            {cadena.charAt(0), cadena.charAt(1), cadena.charAt(2)},
+            {cadena.charAt(3), cadena.charAt(4), cadena.charAt(5)},
+            {cadena.charAt(6), cadena.charAt(7), cadena.charAt(8)}
+        };
+
+        //IMPRESO
+        System.out.println("\n"+lh);
+        System.out.println(linea + tablero[0][0] + tablero[0][1] + tablero[0][2] + linea);
+        System.out.println(linea + tablero[1][0] + tablero[1][1] + tablero[1][2] + linea);
+        System.out.println(linea + tablero[2][0] + tablero[2][1] + tablero[2][2] + linea);
+        System.out.println(lh);
     }
 }
